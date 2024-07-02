a78df51396cbd05aa60460dc3d3a7cd993428673
Philip Soliman <philipsoliman4133@gmail.com>
2024-07-02 19:18:19 +0200

    started conversion to editor type

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
-from .auto_invoice_manager.controller import Controller as autoInvoiceManager
+# from .auto_invoice_manager.controller import Controller as autoInvoiceManager
 from .auto_invoice.controller import Controller as autoInvoice
 
-from viktor import InitialEntity
+# from viktor import InitialEntity
 
-initial_entities = [
-    InitialEntity(
-        "autoInvoiceManager",
-        name="Auto invoice manager",
-        children=[
-            InitialEntity("autoInvoice", name="Auto invoice"),
-        ],
-    ),
-]
+# initial_entities = [
+#     InitialEntity(
+#         "autoInvoiceManager",
+#         name="Auto invoice manager",
+#         children=[
+#             InitialEntity("autoInvoice", name="Auto invoice"),
+#         ],
+#     ),
+# ]
